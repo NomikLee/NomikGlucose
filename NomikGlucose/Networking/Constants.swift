c5ccbe227b617dc5269256f1fc1ceed4d4436111
NomikLee <Nomiklee.icloud.com>
2025-01-21 10:21:11 +0800

    fix bug

--- NomikGlucose/Networking/Constants.swift
+++ NomikGlucose/Networking/Constants.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 struct Constants {
-    static let newsApiKey = "658c3d71ad824579b687c7cc90863efe"
+    static let newsApiKey = ""
     static let newsBaseUrl = "https://newsapi.org/v2/everything"
     
-    static let geminiApiKey = "AIzaSyBJde-G0b1RjPTAg5LcsOemew6wQSa1ei4"
+    static let geminiApiKey = ""
     static let geminiBaseUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
 }
